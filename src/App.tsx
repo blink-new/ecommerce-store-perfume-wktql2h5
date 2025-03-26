915bdd102b4e27131bfa275fb5c8f8eaba2f00e1
ShadowWalker2014 <kai.jiabo.feng@gmail.com>
2025-03-26 12:42:44 +0000

    Add React Router and Setup Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,29 @@
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Header } from './components/layout/Header';
 import { Home } from './pages/Home';
 import { Shop } from './pages/Shop';
 import { ProductDetail } from './pages/ProductDetail';
 import { Cart } from './pages/Cart';
-import { AnimatePresence } from 'framer-motion';
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
 
-export default function App() {
+function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-neutral-50">
-        <Header />
-        <main className="pt-16">
-          <AnimatePresence mode="wait">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/shop" element={<Shop />} />
-              <Route path="/product/:id" element={<ProductDetail />} />
-              <Route path="/cart" element={<Cart />} />
-            </Routes>
-          </AnimatePresence>
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/product/:id" element={<ProductDetail />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
         </main>
+        <Footer />
       </div>
     </Router>
   );
 }
+
+export default App;
